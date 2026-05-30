8efb1f795d20c32967e92d9619ede61270d21805
Dustin <dustinsheldon10000@gmail.com>
2026-05-30 05:34:03 +0000

    updated shopping_list to enumerate items and spacing

--- lists/shopping_list.py
+++ lists/shopping_list.py
@@ -1,19 +1,21 @@
 
 shopping_list = []
+
 while True:
- print("\nShopping List:", shopping_list)
- print("1. Add item")
+ print("\nShopping List: ")
+ for i, item in enumerate(shopping_list, start =1):
+  print(f"\n{i}. {item}")
+ print("\n1. Add item")
  print("2. Remove Item")
  print("3. Quit")
  choose_option = input("Choose an option: 1. 2. 3. ")
-
  if choose_option == "1":
   print("\nAdd Item:")
   added_item = input()
   shopping_list.append(added_item)
  elif choose_option == "2":
   print("\nChoose Item to Remove: ")
-  item_to_remove = int(input()) - 1
+  item_to_remove = int(input()) - 1 
   shopping_list.pop(item_to_remove)
  elif choose_option == "3":
   print("Goodbye!")
