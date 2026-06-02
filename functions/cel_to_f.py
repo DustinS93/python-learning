b8b291c2d48d0820b6d35b89ee31780e3a17359b
Dustin <dustinsheldon10000@gmail.com>
2026-06-02 06:07:19 +0000

    fixed the option to quit by changing where c converts to int

--- functions/cel_to_f.py
+++ functions/cel_to_f.py
@@ -1,7 +1,12 @@
 # c = input("what's the tempurature int celcius? ")
-def c_to_f(c): return (int(c) * 1.8 + 32)
+def c_to_f(c): return (c * 1.8 + 32)
 while True:
 	c = input("what's the temp in celcius? ")
-	print(c_to_f(c))
 	if c == ("quit"):
 		break
+	else:
+		print(c_to_f(int(c)))
+		# c = input("what's the temp in celcius? ")
+	# print(c_to_f(int(c)))
+	# if c == ("quit"):
+		# break
